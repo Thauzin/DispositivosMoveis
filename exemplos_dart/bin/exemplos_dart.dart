62edd53bdf492d7c2d63b456b6e643d7dc9a5a57
Thauzin | dev <thauanyfm3@gmail.com>
2026-03-09 21:45:51 -0300

    Initial commit

---
+++ exemplos_dart/bin/exemplos_dart.dart
@@ -0,0 +1,36 @@
+// import 'package:exemplos_dart/exemplos_dart.dart' as exemplos_dart;
+
+void main() {
+  print('Hello world: ');
+
+  int qtdeProdutos = 10;
+  double precoProduto = 19.99;
+  String nome = "Teclado";
+  bool emEstoque = true;
+  var categoria = "Tech";
+
+  print('int: $qtdeProdutos');
+  print('double: $precoProduto');
+  print('boolean: $emEstoque');
+  print('vari: $categoria');
+  print('String: $nome');
+
+}
+
+class Produto{
+  String nome;
+  double preco;
+  String categoria;
+
+  Produto(this.nome,this.categoria, this.preco);
+}
+
+void exibirproduto(){
+  print('Produto: ${this.nome}' );
+}
+
+void exemploclass(){
+  Produto produto1 = Produto("Xbox", "Tech", 4500);
+  produto1.exibirproduto(); 
+}
+
---
+++ exemplos_dart/test/exemplos_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:exemplos_dart/exemplos_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
