2188af4f28fa4a296783cb7bba2b715af2381aef
Thauzin | dev <thauanyfm3@gmail.com>
2026-03-28 14:44:04 -0300

    [ATT] - Menu adicionado

--- exemplos_dart/bin/ativ2.dart
+++ exemplos_dart/bin/ativ2.dart
@@ -1,13 +1,7 @@
 import 'dart:io';
 
 void main(){
-
-
-  adicionarlivro();
-  exibirlivros();
-  adicionarlivro();
-  removerlivro();
-  exibirlivros();
+  menu();
 }
 
 class Livro{
@@ -67,4 +61,38 @@ void removerlivro(){
     livros.removeAt(indiceremover);
   }
 
+}
+
+void menu(){
+  print("----------------------------");
+  print("Bem vindo a Biblioteca! O que deseja fazer? \n 1 - Adicionar Livro\n 2 - Remover Livro \n 3 - Atualizar Livrvo \n 4 - Listar Livros \n 5 - Sair ");
+  String? opcao = stdin.readLineSync();
+  switch(opcao) {
+    case "1":
+    adicionarlivro();
+    menu();
+    break;
+
+    case "2" :
+    removerlivro();
+    menu();
+    break;
+
+    case "3":
+    print("Ainda não temos essa opção... Volte mais tarde");
+    menu();
+    break;
+
+    case "4" : 
+    exibirlivros();
+    menu();
+    break;
+
+    case "5" :
+    print("Espero que tenha gostado! Obrigada! ");
+
+    default: 
+    print("Opção inválida! Digite as opções disponiveis!");
+    menu();
+  }
 }
